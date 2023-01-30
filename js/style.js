318077299bded421ebdc244e14b603c77acf65a9
techoalok <aokkumarsingh862@gmail.com>
2023-01-30 10:21:04 +0530

    JS DOM

---
+++ js/style.js
@@ -0,0 +1,16 @@
+// document.getElementById('btn').onclick = function(){
+//     alert("Hello")
+// }
+
+// function clickBtn(){
+//     alert("Hello")
+// }
+
+// document.getElementById('btn').addEventListener('click', ()=>{
+//     alert("Hello")
+// })
+
+btn = document.getElementById('btn');
+btn.addEventListener('click', ()=>{
+    document.getElementById('box1').type = "radio"
+})
