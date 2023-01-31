1a2b944533662c6f2dd4b81a627ee5041a38fd93
techoalok <aokkumarsingh862@gmail.com>
2023-01-31 10:27:14 +0530

    input prolem in js exampe

--- js/style.js
+++ js/style.js
@@ -1,16 +1,22 @@
-// document.getElementById('btn').onclick = function(){
-//     alert("Hello")
-// }
-
-// function clickBtn(){
-//     alert("Hello")
-// }
-
-// document.getElementById('btn').addEventListener('click', ()=>{
-//     alert("Hello")
-// })
-
 btn = document.getElementById('btn');
+a1 = document.querySelector("#box1");
+a2 = document.querySelector("#box2");
+disp = document.querySelector("#disp");
+
 btn.addEventListener('click', ()=>{
-    document.getElementById('box1').type = "radio"
+    disp.style.color = "red";
+    if(a1.value==""){
+        disp.innerText = "Box1 is required..."
+        a1.style.border = "1px solid red"
+        a1.title = "Box1 is required..."
+    }else if(a2.value==""){
+        disp.innerText = "Box2 is required..."
+    }else{
+        disp.innerHTML = "Box1 : "+a1.value+"<br>Box2 : "+a2.value;
+        disp.style.color = "green";
+    }
+})
+
+a1.addEventListener('keyup',()=>{
+    a1.style.border = "2px solid green"
 })
